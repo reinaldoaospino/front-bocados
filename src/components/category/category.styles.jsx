a163873e67743c63dd77048c23115e92e3394f49
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-10-11 13:29:07 -0300

    se hace responsive el category

--- src/components/category/category.styles.jsx
+++ src/components/category/category.styles.jsx
@@ -6,6 +6,32 @@ export const CategoryContainer = styled.div`
   grid-template-columns: 12% 12% 12% 12%;
   margin-top: 200px; //TODO eliminar
   text-align: center;
+
+  @media (max-width: 845px) {
+    grid-template-columns: 14% 14% 14% 14%;
+  }
+
+  @media (max-width: 845px) {
+    grid-template-columns: 17% 17% 17% 17%;
+  }
+
+  @media (max-width: 583px) {
+    grid-template-columns: 20% 20% 20% 20%;
+  }
+
+  @media (max-width: 490px) {
+    grid-template-columns: 25% 25%;
+    grid-gap: 14px;
+  }
+
+  @media (max-width: 385px) {
+    grid-template-columns: 28% 28%;
+    grid-gap: 14px;
+  }
+  @media (max-width: 341px) {
+    grid-template-columns: 30% 30%;
+    grid-gap: 14px;
+  }
 `;
 
 export const CategoryWrapped = styled.div`
@@ -14,8 +40,8 @@ export const CategoryWrapped = styled.div`
   color: ${(props) => (props.selected ? "white" : "none")};
   ${(props) => props.color}
   &:hover {
-    background-color: #C5DB74;
-    color:white;
+    background-color: #c5db74;
+    color: white;
     cursor: pointer;
   }
 `;
