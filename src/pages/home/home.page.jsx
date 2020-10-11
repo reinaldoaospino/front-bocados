d8015ee60279fa08bc0e64a7ab75c49ad74a4c71
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-10-11 13:19:25 -0300

    import que no se usa

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -9,7 +9,6 @@ import GlutenFreeIcon from "../../components/gluten-free-icon/gluten-free-icon.c
 import SugarFreeIcon from "../../components/sugar-free-icon/sugar-free-icon.component";
 import KetoIcon from "../../components/keto-icon/keto-icon.component";
 import ProductsPreview from "../../components/products-preview/products-preview.component";
-import Footer from "../../components/footer/footer.component";
 
 const SugarFree = Award(SugarFreeIcon);
 const GlutenFree = Award(GlutenFreeIcon);
