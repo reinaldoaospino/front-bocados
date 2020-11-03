966da72cda75fd4371252b2774a2683263de2b10
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-11-03 16:07:03 -0300

    Create fetching.action.js

---
+++ src/redux/fetching/fetching.action.js
@@ -0,0 +1,16 @@
+import FetchingActionTypes from './fetching.types';
+
+export const setIsFetching = (value) => ({
+    type: FetchingActionTypes.IS_FETCHING,
+    payload: value
+})
+
+export const setFetchingSuccess= (value)=>({
+    type: FetchingActionTypes.FETCHING_SUCCESS,
+    payload: value
+})
+
+export const setFetchingComplete= (value)=>({
+    type: FetchingActionTypes.FETCHING_COMPLETE,
+    payload: value
+})
