27488beece697d7d4cf2c40494c9c5db69b23646
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-11-05 17:07:10 -0300

    Update root-saga.js

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,8 +1,10 @@
 import { all, call } from "redux-saga/effects";
 import { productSagas } from './product/product.sagas';
-import { categorySagas } from './category/category.sagas'
+import { categorySagas } from './category/category.sagas';
+import { subscriptionSagas } from './subscription/subscription.sagas';
+import { sendEmailSagas } from './email/email.sagas';
 
 
 export default function* rootSaga() {
-    yield all([call(productSagas), call(categorySagas)])
+    yield all([call(productSagas), call(categorySagas), call(subscriptionSagas), call(sendEmailSagas)])
 }
