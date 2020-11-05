be102a4cb7872347c28a4c35a8a3d42cc4543bf2
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-11-05 16:01:15 -0300

    Create subscription-service.js

---
+++ src/services/subscription/subscription-service.js
@@ -0,0 +1,20 @@
+import bocadosApi from '../../apis/bocadosApis';
+import { GetToken } from '../token/token-service';
+
+export const CreateSubscription = async (subscriptionData) => {
+    var tokenTask = GetToken();
+
+    const data = JSON.stringify({
+        "Email": subscriptionData.email,
+    })
+
+    var token = await tokenTask;
+
+    const response = await bocadosApi.post('/api/subscription', data, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    return response;
+}
