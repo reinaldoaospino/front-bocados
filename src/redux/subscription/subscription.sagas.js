0798b67c3df51d68624b826695d77aa4bc195ec5
reinaldoaospino <48297872+reinaldoaospino@users.noreply.github.com>
2020-11-05 16:21:37 -0300

    Add files via upload

---
+++ src/redux/subscription/subscription.sagas.js
@@ -0,0 +1,25 @@
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+import SubscriptionActionTypes from './subscription.types';
+import { fetCreateSubscriptionFailure } from './subscription.action'
+import { fechingComplete, feching } from '../helpers/fechingHelper';
+import { CreateSubscription } from '../../services/subscription/subscription-service';
+
+export function* fetchCreateSubscriptionAsync() {
+
+    try {
+        yield put(feching());
+        yield CreateSubscription();
+        yield fechingComplete();
+    } catch (error) {
+        yield put(fetCreateSubscriptionFailure(error))
+    }
+}
+
+export function* fetchCreateSubscriptionStart() {
+    yield takeLatest(SubscriptionActionTypes.FETCH_CREATE_SUBSCRIPTION_START, fetchCreateSubscriptionAsync)
+}
+
+
+export function* subscriptionSagas() {
+    yield call([call(fetchCreateSubscriptionStart)])
+}
