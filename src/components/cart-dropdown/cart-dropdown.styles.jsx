aaef2ebc569b6372512ab913a8be33d1038671d8
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2020-11-11 20:27:42 -0300

    se modifica el car-drop

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -9,7 +9,7 @@ export const CartDropDownContainer = styled.div`
   padding: 20px;
   border: 1px solid black;
   background-color: white;
-  top: 90px;
+  top: 70px;
   right: 40px;
   z-index: 5;
   z-index: 5;
