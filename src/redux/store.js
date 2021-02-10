c00e7ba30efde85707bdad774d72993906a76213
Reinaldo Ospino <reinaldo.aospino@gmail.com>
2021-02-10 17:20:10 -0300

    se agregan cambios varios

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,7 +12,7 @@ const middlewares = [sagaMiddleware];
 if (process.env.NODE_ENV === "development") {
     middlewares.push(logger);
   }
-  
+   
 
 const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
